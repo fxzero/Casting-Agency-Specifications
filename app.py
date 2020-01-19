ec7acd7222f5e7f185ce1946ea708709924fb4e8
Bruce Zhu <fxzero@qq.com>
2020-01-19 08:33:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from models import setup_db, Movie, Actor
 from auth import AuthError, requires_auth
 import datetime
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -16,11 +17,15 @@ def create_app(test_config=None):
 
 app = create_app()
 
+
 @app.after_request
-def after_request(response):     
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
     return response
+
 
 @app.route('/actors')
 @requires_auth('get:actors')
@@ -36,6 +41,7 @@ def get_actors(jwt):
         'actors': actors_list
     })
 
+
 @app.route('/actors', methods=['POST'])
 @requires_auth('post:actors')
 def add_actor(jwt):
@@ -49,11 +55,12 @@ def add_actor(jwt):
 
     actor = Actor(name=req_name, age=req_age, gender=req_gender)
     actor.insert()
-    
+
     return jsonify({
         'success': True,
         'actors': actor.format()
     })
+
 
 @app.route('/actors/<id>', methods=['PATCH'])
 @requires_auth('patch:actors')
@@ -81,6 +88,7 @@ def update_actor(jwt, id):
         'actors': actor.format()
     })
 
+
 @app.route('/actors/<id>', methods=['DELETE'])
 @requires_auth('delete:actors')
 def delete_actor(jwt, id):
@@ -93,6 +101,7 @@ def delete_actor(jwt, id):
         'success': True,
         'actors': id
     })
+
 
 @app.route('/movies')
 @requires_auth('get:movies')
@@ -108,23 +117,26 @@ def get_movies(jwt):
         'movies': movies_list
     })
 
+
 @app.route('/movies', methods=['POST'])
 @requires_auth('post:movies')
 def add_movie(jwt):
     body = request.get_json()
     req_title = body.get('title')
     req_release_date = body.get('release_date')
+    release_date = datetime.datetime.strptime(req_release_date, "%Y-%m-%d")
 
     if not req_title:
         abort(422)
 
-    movie = Movie(title=req_title, release_date=datetime.datetime.strptime(req_release_date, "%Y-%m-%d"))
+    movie = Movie(title=req_title, release_date=release_date)
     movie.insert()
-    
+
     return jsonify({
         'success': True,
         'movies': movie.format()
     })
+
 
 @app.route('/movies/<id>', methods=['PATCH'])
 @requires_auth('patch:movies')
@@ -134,6 +146,7 @@ def update_movie(jwt, id):
     req_release_date = body.get('release_date')
     if (not req_title) and (not req_release_date):
         abort(422)
+    release_date = datetime.datetime.strptime(req_release_date, "%Y-%m-%d")
 
     movie = Movie.query.filter(Movie.id == id).one_or_none()
     if not movie:
@@ -141,13 +154,14 @@ def update_movie(jwt, id):
     if req_title:
         movie.title = req_title
     if req_release_date:
-        movie.release_date = datetime.datetime.strptime(req_release_date, "%Y-%m-%d")
+        movie.release_date = release_date
     movie.update()
 
     return jsonify({
         'success': True,
         'movies': movie.format()
     })
+
 
 @app.route('/movies/<id>', methods=['DELETE'])
 @requires_auth('delete:movies')
@@ -162,34 +176,38 @@ def delete_movie(jwt, id):
         'movies': id
     })
 
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 404,
         "message": "resource not found"
     }), 404
 
+
 @app.errorhandler(405)
 def not_allowed(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 405,
         "message": "Method not allowed"
     }), 405
 
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 422,
         "message": "unprocessable"
     }), 422
 
+
 @app.errorhandler(500)
 def server_error(error):
     return jsonify({
-        "success": False, 
+        "success": False,
         "error": 500,
         "message": "Server error"
     }), 500
@@ -202,6 +220,10 @@ def auth_error(error):
         "message": error.error
     }), error.status_code
 
+
 if __name__ == '__main__':
     app.run()
+<<<<<<< HEAD
+=======
     
+>>>>>>> d1fab5fd685cf980f9b55fe28535442511782852
